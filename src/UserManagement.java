e8a0e3ee5d0b2b03c5a4ab59c5b5afc5a0b3c823
Eric Leask <16efl@queensu.ca>
2020-08-05 13:56:18 -0400

    Create UserManagement.java

---
+++ src/UserManagement.java
@@ -0,0 +1,8 @@
+public class UserManagement{
+    public void AddUser(){}
+    public void AddUsers(){}
+    public void RemoveUser(){}
+    public void RemoveUsers(){}
+    public void UpdateUser(){}
+    public void ListUsers(){}
+}
