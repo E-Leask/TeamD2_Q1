7df0b8f221053ade1a7a456ec294c67ae653c8a3
EraYIF <44327129+EraYIF@users.noreply.github.com>
2020-08-05 12:25:30 -0400

    Add files via upload

---
+++ src/ServiceAccount.java
@@ -0,0 +1,42 @@
+package mobileservice;
+
+import java.util.*;
+
+class ServiceAccount {
+	ArrayList<UserAccount> user = new ArrayList<UserAccount>();
+	ArrayList<Integer> phoneNumber = new ArrayList<Integer>();
+	ServicePackage servicePackage;
+	int monthlyFees;
+	
+	public ServiceAccount(ServicePackage servicePackage) {
+		this.servicePackage = servicePackage;
+	}
+	
+	public void addUser(UserAccount user) {
+		this.user.add(user);
+		System.out.println("User " + user + " is added to the service account");
+	}
+	
+	public void removeUser(UserAccount user) {
+		if (this.user.contains(user)==true) {
+			this.user.remove(user);
+			System.out.println("User " + user +" is removed");
+		} else {
+			System.out.println("User doesn't exist in this service account");
+		}
+	}
+	
+	public void addPhoneNumber(int phoneNumber) {
+		this.phoneNumber.add(phoneNumber);
+		System.out.println("Phone number: " + phoneNumber + " is added to the service account");
+	}
+	
+	public void removePhoneNumber(int phoneNumber) {
+		if (this.phoneNumber.contains(phoneNumber) == true) {
+			this.phoneNumber.remove(phoneNumber);
+			System.out.println("Phone number " + phoneNumber + " is removed");
+		} else {
+			System.out.println("Phone number doesn't exist in this service account");
+		}
+	}
+}
---
+++ src/UserAccount.java
@@ -0,0 +1,19 @@
+package mobileservice;
+
+class UserAccount{
+	String userName;
+	String userAddress;
+	String userEmail;
+	
+	public UserAccount(String userName, String userAddress, String userEmail) {
+		this.userName = userName;
+		this.userAddress = userAddress;
+		this.userEmail = userEmail;
+	}
+	
+	public void printUserAccount() {
+		System.out.println("User Name: " + userName);
+		System.out.println("User Address: " + userAddress);
+		System.out.println("User Email: " + userEmail);
+	}
+}
