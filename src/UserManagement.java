4602f6725f5d76376e7fe71dc87c9d27fe556f7e
Eric Leask <16efl@queensu.ca>
2020-08-06 14:30:01 -0400

    Update UserManagement.java

--- src/UserManagement.java
+++ src/UserManagement.java
@@ -1,8 +1,42 @@
+import java.util.*;
+
 public class UserManagement{
-    public void AddUser(){}
-    public void AddUsers(){}
-    public void RemoveUser(){}
-    public void RemoveUsers(){}
-    public void UpdateUser(){}
-    public void ListUsers(){}
+    
+    public static ServiceAccount AddUser(ServiceAccount account,UserAccount user){
+        account.addUser(user);
+        return account;
+    }
+    public static ServiceAccount AddUsers(ServiceAccount account,ArrayList<UserAccount> users){
+        Iterator<UserAccount> user = users.iterator();
+        while (user.hasNext()) {
+            account.addUser(user);
+        }
+        return account;
+    }
+    public static ServiceAccount RemoveUser(ServiceAccount account,UserAccount user){
+        account.removeUser(user);
+        return account;
+    }
+    public static ServiceAccount RemoveUsers(ServiceAccount account,ArrayList<UserAccount> users){
+        Iterator<UserAccount> user = users.iterator();
+        while (user.hasNext()) {
+            account.removeUser(user);
+        }
+        return account;
+    }
+    public static ServiceAccount UpdateUser(ServiceAccount account,UserAccount originalUser,UserAccount replacementUser){
+        	if (account.user.contains(OriginalUser)==true) {
+			account.user=replacementUser;
+			System.out.println("User " + user +" is updated");
+		} else {
+			System.out.println("User doesn't exist in this service account");
+		}
+        return account;
+    }
+    public void ListUsers(ServiceAccount account){
+    Iterator<UserAccount> user = users.iterator();
+        while (user.hasNext()) {
+            user.printUserAccount(); //
+        }    
+    }
 }
